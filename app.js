4e8e738473905a380e0fd939ebc8d8f7b5839a34
Gaurav_Singh <gauravkarki038@gmail.com>
2023-03-26 22:33:13 +0530

    expiry update1

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ const cors = require('cors')
 
 const errorMiddleware = require("./middleware/error")
 
-
-require("dotenv").config({ path: "./config/config.env" });
-app.use(express.json())
-app.use(cookieParser())
 app.use(cors({
     credentials: true,
     origin: true,
     optionsSuccessStatus: 200,
   }))
+require("dotenv").config({ path: "./config/config.env" });
+app.use(express.json())
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
